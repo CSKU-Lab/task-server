e24ff99c9981ab71764181806ec8cc5c5be7cf95
SornchaiTheDev <sornchaithedev@gmail.com>
2026-01-19 17:47:40 +0700

    feat: add context with timeout to terminate generate test case when go-grader has some issue

--- cmd/grpc/grpc.go
+++ cmd/grpc/grpc.go
@@ -321,6 +321,9 @@ func (g *grpcServer) UpdateTask(ctx context.Context, req *pb.UpdateTaskRequest) 
 
 				testcases := praseTestCasesPBToModel(group.GetTestCases())
 
+				ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+				defer cancel()
+
 				updateTestCases, err := g.generateTestCases(ctx, generateTestCasesPayload{
 					solutionRunnerID: req.GetSolutionRunnerId(),
 					solutionFiles:    solutionFiles,
